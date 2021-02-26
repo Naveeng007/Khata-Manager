012af958690f4f15907c06a25bf401a6e99903dd
Naveen <thisismenaveenkumar@gmail.com>
2021-02-27 01:05:52 +0530

    port added in server.js

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,8 @@ module.exports=(env)=>{
         }),
         // CSSExtract
     ],
-    devtool:isProduction?'inline-source-map':'eval-cheap-module-source-map',
+    // devtool:isProduction?'inline-source-map':'eval-cheap-module-source-map',
+     devtool:isProduction?false:'eval-cheap-module-source-map'
     // devtool:false //use it for production
     
     // devtool: 'inline-source-map',i have seen on stackoverflow for better production
